17856482ba883b59ae2e473a145c497005e8914b
Patrick Choi <ptrckchoi@gmail.com>
2023-05-02 15:51:57 -0700

    added some css for mobile

---
+++ src/components/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import MainContent from './MainContent/MainContent'
+import React from 'react'
+const HomeScreen:React.FC = () => {
+
+  return (
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  )
+  
+}
+
+export default HomeScreen;
